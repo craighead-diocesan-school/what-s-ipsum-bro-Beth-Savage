746849697252ca18941047f7213d3aa589087de6
Beth-Savage <145308916+Beth-Savage@users.noreply.github.com>
2025-02-24 14:18:38 +1300

    Styled Header Component
    Added a global font and CSS styles to the header component

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,16 @@
 <header>
   <h1>What's Ipsum, Bro?</h1>
 </header>
+<style>
+  header{
+    background-color: rgb(189, 161, 216);
+    text-align: center;
+  }
+  h1{
+    font-family:'Indie Flower';
+    font-weight: bold;
+    color: rgb(216, 253, 241);
+    margin-top: 100px;
+    margin-bottom: 0;
+  }
+</style>
